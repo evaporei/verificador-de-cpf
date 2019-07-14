32763054e413e339427b849ee89aabefd06cabc3
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2019-07-14 19:03:19 -0300

    front: add cpf actions

--- static/src/App.js
+++ static/src/App.js
@@ -1,46 +1,102 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import CpfInput from './components/cpfInput'
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.handleCpfChange = this.handleCpfChange.bind(this)
+    this.onSend = this.onSend.bind(this)
+    this.onCheck = this.onCheck.bind(this)
+    this.onRemove = this.onRemove.bind(this)
+    this.onCpfChange = this.onCpfChange.bind(this)
+    this.getStatus = this.getStatus.bind(this)
 
     this.state = {
       cpf: '',
       blacklisted: undefined,
+      message: ''
     }
   }
 
-  onSubmit (event) {
-    event.preventDefault()
-    console.log(this.state)
+  onSend () {
+    const { cpf } = this.state
+    axios.post('/v1/cpf', { cpf })
+      .then((response) => {
+        const { blacklisted } = response.data
+        const message = `CPF ${cpf} foi adicionado à lista negra.`
+        this.setState({ blacklisted, message })
+      })
+      .catch((error) => {
+        const { status } = error.response
+        let message = 'Ocorreu um erro ao tentar enviar o CPF.'
+        if (status === 400) message = 'CPF inválido.'
+        return this.setState({ message })
+      })
   }
 
-  handleCpfChange (event) {
-    event.preventDefault()
-    this.setState({
-      cpf: event.target.value
-    })
+  onCheck () {
+    const { cpf } = this.state
+    axios.get(`/v1/cpf?cpf=${cpf}`)
+      .then((response) => {
+        const { blacklisted } = response.data
+        this.setState({ blacklisted, message: '' })
+      })
+      .catch((error) => {
+        const { status } = error.response
+        let message = 'Ocorreu um erro ao tentar checar o CPF.'
+        if (status === 400) message = 'CPF inválido.'
+        return this.setState({ message })
+      })
   }
 
+  onRemove () {
+    const { cpf } = this.state
+    axios.delete(`/v1/cpf?cpf=${cpf}`)
+      .then((response) => {
+        const { blacklisted } = response.data
+        const message = `CPF ${cpf} foi removido da lista negra.`
+        this.setState({ blacklisted, message })
+      })
+      .catch((error) => {
+        const { status } = error.response
+        let message = 'Ocorreu um erro ao tentar remover o CPF.'
+        if (status === 400) message = 'CPF inválido.'
+        return this.setState({ message })
+      })
+  }
+
+  onCpfChange (event) {
+    event.preventDefault()
+    const { value } = event.target
+    this.setState({ cpf: value, blacklisted: undefined, message: '' })
+  }
+
+  getStatus () {
+    const { blacklisted } = this.state
+    if (blacklisted === true) return 'BLOCK'
+    if (blacklisted === false) return 'FREE'
+    return ''
+  }
 
   render() {
+    const { cpf, message } = this.state
+
     return (
       <div style={{ width: '80%', margin: 'auto', padding: 20}}>
-        <form onSubmit={this.onSubmit}>
-          <label>CPF:</label>
-          <br />
-          <CpfInput
-            type="text"
-            name="cpf"
-            value={this.state.cpf}
-            onChange={this.handleCpfChange}
-          />
-          <br />
-          <button type="submit">Enviar</button>
-        </form>
+        <label>CPF:</label>
+        <br />
+        <CpfInput
+          type="text"
+          name="cpf"
+          value={cpf}
+          onChange={this.onCpfChange}
+        />
+        <br />
+        <button onClick={this.onCheck}>Checar</button>
+        <button onClick={this.onSend}>Adicionar</button>
+        <button onClick={this.onRemove}>Remover</button>
+        <p>{ this.getStatus() }</p>
+        <p>{ message }</p>
       </div>
     )
   }
